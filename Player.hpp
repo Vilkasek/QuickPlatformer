7ba8d04ff020d5c14a9567cf7b31b65364c0028a
Jakub Brandt <tv.brandon.nr1@gmail.com>
2023-07-22 13:08:26 +0200

    Prepared player variables

--- Player.hpp
+++ Player.hpp
@@ -3,5 +3,14 @@
 // Player structure
 struct Player
 {
+    // Texture and collision box
+    Texture2D tex = { 0 };
+    Rectangle collBox = { 0 };
 
+    // Movement velocity
+    Vector2 velocity = { 0 };
+
+    // Speed and gravity
+    float speed = 10.f;
+    float grav = 1.f;
 };
